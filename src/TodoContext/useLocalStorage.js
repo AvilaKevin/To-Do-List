79e12c40407561ac68764bf09ae33a197826df70
ViperDex <viperdex2000@hotmailc.om>
2022-07-19 16:13:29 -0500

    useLocalStorage

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -1,46 +1,47 @@
-import React from "react";
+import React from 'react';
 
 function useLocalStorage(itemName, initialValue) {
-  // ESTATES
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const [item, setItem] = React.useState(initialValue);
 
-  //TO DO LOADING AND ERROR
   React.useEffect(() => {
     setTimeout(() => {
       try {
-        const localStorageTodos = localStorage.getItem(itemName);
+        const localStorageItem = localStorage.getItem(itemName);
         let parsedItem;
 
-        if (!localStorageTodos) {
+        if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItem = initialValue;
         } else {
-          parsedItem = JSON.parse(localStorageTodos);
+          parsedItem = JSON.parse(localStorageItem);
         }
 
         setItem(parsedItem);
         setLoading(false);
       } catch (error) {
-        setError(true);
+        setError(error);
       }
-
-    }, 1000, []);
+    }, 1000);
   });
 
-  //TO DO SAVE ITEM
   const saveItem = (newItem) => {
     try {
       const stringifiedItem = JSON.stringify(newItem);
       localStorage.setItem(itemName, stringifiedItem);
       setItem(newItem);
     } catch (error) {
-      setError(true);
+      setError(error);
     }
   };
 
-  return { item, saveItem, loading, error };
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
 }
 
 export { useLocalStorage };
